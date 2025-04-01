6d5242fd261dd272a472ae2b2ac7685dcb115517
Nayak <likhitnayak@gmail.com>
2025-04-02 00:29:17 +0530

    slight formatting changes

--- app/physics/ib_dp_physics_hl/thermodynamics/learn_by_teaching/q1/question.py
+++ app/physics/ib_dp_physics_hl/thermodynamics/learn_by_teaching/q1/question.py
@@ -5,7 +5,7 @@ temperature_initial = insert_latex("300\\:K")
 temperature_final = insert_latex("200\\:K")
 num_moles = insert_latex("20\\:mol")
 
-question = ("{num_moles} of an ideal gas undergoes adiabatic expansion. Initially, the gas is at a temperature of {temperature_initial} and finally, the temperature of the gas is {temperature_final}. What is the total work done by the gas?").format(topic=topic, num_moles=num_moles, temperature_initial=temperature_initial, temperature_final=temperature_final)
+question = ("{num_moles} of an ideal gas undergoes adiabatic expansion. Initially, the gas is at a temperature of {temperature_initial} and finally, the temperature of the gas is {temperature_final}. What is the total work done by the gas?").format(num_moles=num_moles, temperature_initial=temperature_initial, temperature_final=temperature_final)
 answer_type = "single_correct"
 
 answer_output = insert_latex("24942\\:J")
